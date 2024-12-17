764e35890a0a95e77f56f194b5c9e726f120ba51
Wang Zixuan <61129921+hahahawzx@users.noreply.github.com>
2024-12-18 00:18:19 +0800

    fix(rl): remove train files

--- controller/rl_train/temp.py
+++
@@ -1,5 +0,0 @@
-import pstats
-
-with open("profile_results.prof", "r") as file:
-    stats = pstats.Stats("profile_results.prof")
-    stats.sort_stats("cumulative").print_stats(10)  # Top 10 time-consuming calls
